91737d3e64f782a7417534e68cd12ab66d562065
Dragana Begojev <dragana.begojev@gmail.com>
2026-01-05 20:42:23 +0100

    Set up the Book Routes and Views
    Create Book Route
    Route: post /books/new - Posts a new book to the database
    
    Book Detail Route
    Route: get /books/:id - Shows book detail form
    View: update_book.pug
    
    Update Book Route
    Route: put /books/:id - Updates book info in the database

--- routes/books.js
+++ routes/books.js
@@ -17,6 +17,69 @@ router.get('/new', (req, res) => {
   res.render('books/new_book', { title: 'New Book' });
 });
 
+// POST /books/new - create a new book
+router.post('/new', async (req, res, next) => {
+  try {
+    await Book.create(req.body);
+    res.redirect('/books');
+  } catch (err) {
+    if (err.name === 'SequelizeValidationError') {
+      res.render('books/new_book', {
+        title: 'New Book',
+        book: req.body,
+        errors: err.errors
+      });
+    } else {
+      next(err);
+    }
+  }
+});
+
+// GET /books/:id - book detail
+router.get('/:id', async (req, res, next) => {
+  try {
+    const book = await Book.findByPk(req.params.id);
+    if (book) {
+      res.render('books/update_book', { 
+        title: book.title, 
+        book 
+      });
+    } else {
+      const err = new Error('Book not found');
+      err.status = 404;
+      throw err;
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+// POST /books/:id - update book
+router.put('/:id', async (req, res, next) => {
+  try {
+    const book = await Book.findByPk(req.params.id);
+
+    if (!book) {
+      const err = new Error('Book not found');
+      err.status = 404;
+      throw err;
+    }
+
+    await book.update(req.body);
+    res.redirect('/books');
+  } catch (err) {
+    if (err.name === 'SequelizeValidationError') {
+      const book = await Book.findByPk(req.params.id);
+      res.render('books/update_book', {
+        title: book.title,
+        book,
+        errors: err.errors
+      });
+    } else {
+      next(err);
+    }
+  }
+});
+
+
 module.exports = router;
-
-
